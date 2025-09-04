fe5856b90a6cc5ed2de259054cdf0282def5ad64
nmandell2022 <nmandell2022@gmail.com>
2025-09-04 15:44:10 -0400

    Initial project files

---
+++ src/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import VelvetNights from './App'
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <VelvetNights />
+  </React.StrictMode>
+)
